f2ae11626f4fc911efdf36806c49b17d2d3b24b5
Ashley Mayle <amayle@cs.unm.edu>
2018-03-17 20:06:25 -0600

    Adding Food bank data

--- GUIStuff/src/FoodBank.java
+++ GUIStuff/src/FoodBank.java
@@ -4,17 +4,22 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class FoodBank {
 
     Button back;
     BorderPane bpane;
+    ArrayList<Button> buttons;
     public FoodBank(Stage primaryStage) {
         bpane = new BorderPane();
         bpane.setStyle("-fx-background-color: #ffffff;");
@@ -40,7 +45,31 @@ public class FoodBank {
         top2.setAlignment(Pos.CENTER);
         left2.setAlignment(Pos.TOP_CENTER);
         right2.setAlignment(Pos.TOP_CENTER);
+        buttons = new ArrayList<Button>();
+        for (int i = 1; i <= 4; i++) {
+            final int b = i;
+            buttons.add(new Button("" + Foodsources.enumTitle(i)));
+            buttons.get(i-1).setStyle("-fx-background-color: #A3D5F7; " +
+                    "-fx-border-color: #000000; -fx-border-width: 1px;");
+            buttons.get(i-1).setMaxSize(200, 50);
+            buttons.get(i - 1).setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent e) {
+                    right2.getChildren().clear();
+                    ScrollPane container = new ScrollPane();
+                    Text info = new Text(Foodsources.enumBody(b));
+                    Text title = new Text(Foodsources.enumTitle(b));
+                    info.setWrappingWidth(550);
+                    container.setContent(info);
+                    right2.getChildren().addAll(container);
+                    top2.getChildren().clear();
+                    top2.getChildren().addAll(title);
+                }
 
+            });
+            left2.getChildren().add(buttons.get(i-1));
+
+        }
 
         bpane.setTop(top2);
         bpane.setLeft(left2);
---
+++ GUIStuff/src/Foodsources.java
@@ -0,0 +1,162 @@
+import java.io.IOException;
+import javafx.scene.text.Text;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class Foodsources {
+
+
+    private static final String abqStoreHouseUrl = "https://www.thestorehouseabq.org/";
+    private static final String roadRunnerUrl = "http://www.rrfb.org/?gclid=CjwKCAjw-bLVBRBMEiwAmKSB83FkyN2iYdg4AhqoUogbwe28xHJmgF8MGEjUuJBE6ofW2tL8Nkoq_BoCOqMQAvD_BwE";
+    private static final String foodProject = "http://www.rgfp.org/";
+    private static final String unitedFoodUrl = "http://feedindiana.org/food-pantries/first-united-methodist-church-food-pantry";
+    private static final String comidaUrl = "http://www.fnch.org/services.aspx?cb9791bccdf34a75bd905ee4b0ad0993blogPostId=d39b26cf9e40424e8ccbf801b4d8d784";
+    private static final String rioGrandeUrl = "https://www.foodpantries.org/li/rio_grande_food_project_87121";
+    private static final String goodShepardUrl = "https://www.goodshepherdcenternm.org/services";
+
+
+
+    public static String  storeHouseTitle() throws IOException {
+        String url = abqStoreHouseUrl;
+        Document document = Jsoup.connect(url).get();
+        String title = document.title();
+        return title;
+    }
+    public static String  storeHouseBody() throws IOException {
+        String url = abqStoreHouseUrl;
+        Document document = Jsoup.connect(url).get();
+        String body = document.body().text();
+        return body;
+    }
+
+    public static String foodRunnerTitle() throws IOException {
+        String url = roadRunnerUrl;
+        Document document = Jsoup.connect(url).get();
+        String title = document.title();
+        return title;
+    }
+    public static String foodRunnerBody() throws IOException {
+        String url = roadRunnerUrl;
+        Document document = Jsoup.connect(url).get();
+        String body = document.body().text();
+        return body;
+    }
+
+    public static String  foodProjectTitle() throws IOException {
+        String url = foodProject;
+        Document document = Jsoup.connect(url).get();
+        String title = document.title();
+        return title;
+    }
+    public static String  foodProjectBody() throws IOException {
+        String url = foodProject;
+        Document document = Jsoup.connect(url).get();
+        String body = document.body().text();
+        return body;
+    }
+
+    public static String  unitedMethodistFoodPantryTitle() throws IOException {
+        String url = unitedFoodUrl;
+        Document document = Jsoup.connect(url).get();
+        String title = document.title();
+        return title;
+    }
+    public static String  unitedMethodistFoodPantryBody() throws IOException {
+        String url = unitedFoodUrl;
+        Document document = Jsoup.connect(url).get();
+        String body = document.body().text();
+        return body;
+    }
+
+//    public static String comidaFoodPantryTitle() throws IOException {
+//        String url = comidaUrl;
+//        Document document = Jsoup.connect(url).get();
+//        String title = document.title();
+//        return title;
+//    }
+//    public static String comidaFoodPantryBody() throws IOException {
+//        String url = comidaUrl;
+//        Document document = Jsoup.connect(url).get();
+//        String body = document.body().text();
+//        return body;
+//    }
+
+//    public static String rioGrandeFoodPantryTitle() throws IOException {
+//        String url = rioGrandeUrl;
+//        Document document = Jsoup.connect(url).get();
+//        String title = document.title();
+//        return title;
+//    }
+//    public static String rioGrandeFoodPantryBody() throws IOException {
+//        String url = rioGrandeUrl;
+//        Document document = Jsoup.connect(url).get();
+//        String body = document.body().text();
+//        return body;
+//    }
+//    public static String goodShepardTitle() throws IOException {
+//        String url = goodShepardUrl;
+//        Document document = Jsoup.connect(url).get();
+//        String title = document.title();
+//        return title;
+//    }
+//    public static String goodShepardBody() throws IOException {
+//        String url = goodShepardUrl;
+//        Document document = Jsoup.connect(url).get();
+//        String body = document.body().text();
+//        return body;
+//    }
+    public static String enumTitle(int i){
+        try {
+            switch (i) {
+                case 1:
+                    return storeHouseTitle();
+                case 2:
+                    return foodRunnerTitle();
+                case 3:
+                    return foodProjectTitle();
+                case 4:
+                    return unitedMethodistFoodPantryTitle();
+//                case 5:
+//                    return comidaFoodPantryTitle();
+//                case 6:
+//                    return rioGrandeFoodPantryTitle();
+//                case 7:
+//                    return goodShepardTitle();
+            }
+        }
+        catch(IOException e){
+            System.out.println("BROKEN LINK FOR TITLES!!");
+            System.exit(1);
+        }
+        return "HOW DID YOU GET HERE!!!";
+
+    }
+
+    public static String enumBody(int i){
+        try {
+            switch (i) {
+                case 1:
+                    return storeHouseBody();
+                case 2:
+                    return foodRunnerBody();
+                case 3:
+                    return foodProjectBody();
+                case 4:
+                    return unitedMethodistFoodPantryBody();
+//                case 5:
+//                    return comidaFoodPantryBody();
+//                case 6:
+//                    return rioGrandeFoodPantryBody();
+//                case 7:
+//                    return goodShepardBody();
+            }
+        }
+        catch(IOException e){
+            System.out.println("BROKEN LINK FOR TITLES!!");
+            System.exit(1);
+        }
+        return "HOW DID YOU GET HERE!!!";
+
+    }
+
+}
